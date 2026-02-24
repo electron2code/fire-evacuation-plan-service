6ae05621f8d90151e31f580ab29777015b2dfdb8
Electron2Code <dev.israfil.code@gmail.com>
2026-02-24 15:57:58 +0600

    remove font from metadata

--- app/(home)/services/opengraph-image.tsx
+++ app/(home)/services/opengraph-image.tsx
@@ -1,16 +1,11 @@
 import prisma from '@/lib/prisma';
 import { ImageResponse } from 'next/og';
-import { readFile } from 'node:fs/promises'
-import { join } from 'node:path'
 
 export const alt = 'Evacuation Plan Services Image'
 export const size = { width: 1200, height: 630 };
 export const contentType = 'image/png';
 
 export default async function Image() {
-    const interSemiBold = await readFile(
-        join(process.cwd(), 'assets/Inter-SemiBold.ttf')
-    )
     // Replace this with your actual data fetching
     const services = await prisma.service.findMany({
         take: 3,
@@ -58,14 +53,6 @@ export default async function Image() {
         ),
         {
             ...size,
-            fonts: [
-                {
-                    name: 'Inter',
-                    data: interSemiBold,
-                    style: 'normal',
-                    weight: 400,
-                },
-            ],
         }
     );
 }

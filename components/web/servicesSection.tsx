209f8808564cfb34e042009b9bae5fa0f2a9db46
electron2code <dev.israfil.code@gmail.com>
2026-04-13 14:47:19 +0600

    omit the full screen height in service section of home page

--- components/web/servicesSection.tsx
+++ components/web/servicesSection.tsx
@@ -6,7 +6,7 @@ import ServiceSkeleton from './service-skeleton';
 export default async function OurServices() {
     return (
         <section className="bg-gray-50">
-            <div className="max-w-7xl py-16 px-4 sm:px-6 lg:px-8 mx-auto min-h-screen">
+            <div className="max-w-7xl py-16 px-4 sm:px-6 lg:px-8 mx-auto">
                 <div className="text-center mb-6 md:mb-8">
                     <h2 className="text-4xl font-bold text-orange-600 mb-3">
                         Evacuation Plan Services

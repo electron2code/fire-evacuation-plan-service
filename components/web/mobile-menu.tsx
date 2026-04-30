10b10f3fc51a84e2d7b4720f45f02502d9ac1f85
electron2code <dev.israfil.code@gmail.com>
2026-04-30 10:05:22 +0600

    test mobile menu

--- components/web/mobile-menu.tsx
+++ components/web/mobile-menu.tsx
@@ -23,7 +23,7 @@ const links = [
         icon: <BoxesIcon className="w-4 h-4 text-red-600 group-hover:text-white" />,
         name: "Services",
         href: "/services",
-        
+
     },
     {
         icon: <FolderCode className="w-4 h-4 text-red-600 group-hover:text-white" />,
@@ -39,44 +39,8 @@ const links = [
 
 export function MobileMenu() {
     return (
-        <Drawer direction="right">
-            <DrawerTrigger asChild>
-                <Button variant="outline"><Menu className="size-6" /></Button>
-            </DrawerTrigger>
-            <DrawerContent>
-                <div className="mx-auto w-full max-w-sm">
-                    <DrawerHeader className="hidden">
-                        <DrawerTitle>Menu</DrawerTitle>
-                        <DrawerDescription></DrawerDescription>
-                    </DrawerHeader>
-                    <div className="p-4 pb-0">
-                        <div className="mt-3 flex flex-col gap-4">
-                            {
-                                links.map((link) => (
-                                    <DrawerClose asChild key={link.name}>
-                                        <Link href={link.href} className="flex justify-center gap-2 items-center text-gray-700 hover:bg-black/90 rounded border border-black/50 px-3 py-2 transition group">
-                                            {link.icon}
-                                            <div>
-                                                <p className="text-xs text-gray-700 group-hover:text-white">{link.name}</p>
-                                            </div>
-                                        </Link>
-                                    </DrawerClose>
-                                ))
-                            }
-                            <DrawerClose asChild>
-                                <Link href="https://wa.me/+8801601770053" target="_blank" className="flex justify-center gap-2 items-center text-gray-700 hover:bg-[#eb6635] rounded border border-black/50 px-3 py-2 transition group">
-                                    <Phone className="w-4 h-4 text-red-600 group-hover:text-white" />
-                                    <div>
-                                        <p className="text-xs text-gray-700 group-hover:text-white">Whatsapp Us Now</p>
-                                    </div>
-                                </Link>
-                            </DrawerClose>
-                        </div>
-                    </div>
-                    <DrawerFooter className="hidden">
-                    </DrawerFooter>
-                </div>
-            </DrawerContent>
-        </Drawer>
+        <div className="flex flex-col">
+            
+        </div>
     )
 }
